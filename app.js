ed1454c99c2344ed3ca8b5ba1d23063cd58f640f
vikyw89 <vikyw89@gmail.com>
2022-12-13 16:12:21 +0800

    rafactor code

--- app.js
+++ app.js
@@ -1,71 +1,135 @@
+const pubSub = (() => {
+    let events = {}
+    const on = (eventName, fn) => {
+      events[eventName] = events[eventName] || [];
+      events[eventName].push(fn);
+    }
+    const off = (eventName, fn) => {
+      if (events[eventName]) {
+        for (let i = 0; i < events[eventName].length; i++) {
+          if (events[eventName][i] === fn) {
+            events[eventName].splice(i, 1);
+            break;
+          }
+        }
+      }
+    }
+    const emit = (eventName, data) => {
+      if (events[eventName]) {
+        events[eventName].forEach(function(fn) {
+          fn(data);
+        });
+      }
+    }
+    return {
+        on,
+        off,
+        emit
+    }
+})()
+
+const player = (arg) => {
+    const name = arg
+    let score = 0
+
+    // cache DOM
+    const playerScore = document.querySelector(`.player-${name.toLowerCase()}-score`)
+
+    // methods
+    const render = () => {
+        playerScore.textContent = score
+    }
+
+    const setScore = (winner)=> {
+        if (name === winner) {
+            score++
+            render()
+        }
+    }
+
+    // bind events
+    pubSub.on('playerWin', setScore)
+    
+    // init
+    render()
+}
+
+const playerX = player('X')
+const playerO = player('O')
+
+
 const gameBoard = (()=> {
     let gameStats = [
         [null,null,null],
         [null,null,null],
         [null,null,null]
     ]
-    let history = []
+    const players = ['X', 'O']
+    let turnCount = 0
 
     // cache dom
     const board = document.querySelector('.tictactoe-container')
-    const announcementBoard = document.querySelector('.info')
+    const cells = document.querySelectorAll('.cell')
     
-    // bind events
-    board.addEventListener('click', boardClickHandler)
-
-    render()
-
     // methods
-    function render() {
+    const render =()=> {
         board.innerHTML = ''
-        let tempBoard = document.createElement('div')
         for(let row = 0; row < gameStats.length; row++){
             for (let col = 0; col < gameStats[row].length; col++){
                 const newCell = document.createElement('div')
                 newCell.setAttribute('data-col', col)
                 newCell.setAttribute('data-row', row)
+                newCell.classList.add('cell')
                 newCell.textContent = gameStats[row][col]
+                if (newCell.textContent === 'X') {
+                    newCell.setAttribute('class', 'font-effect-fire')
+                } else if (newCell.textContent === 'O') {
+                    newCell.setAttribute('class', 'font-effect-neon')
+                }
                 board.appendChild(newCell)
             }
         }
     }
 
-    function boardClickHandler(e) {
-        if (gameStats[e.target.dataset.row][e.target.dataset.col] !== null) {
-            return null
-        }
-        const playerTurn = history[history.length-1] === 'X'
-            ? 'O'
-            : 'X'
-        history.push(playerTurn)
+    const boardClickHandler =(e)=> {
+        if (gameStats[e.target.dataset.row][e.target.dataset.col] !== null) return 
+        const playerTurn = players[turnCount%2]
+        turnCount++
         gameStats[e.target.dataset.row][e.target.dataset.col] = playerTurn
         render()
-        if (history.length >= 5) {
-            if (checkWinner() === true) {
-                announceWinner()
-                resetBoard()
-                if (playerTurn === 'X') {
-                    playerX.win()
-                    playerX.scoreInfo()
-                } else if (playerTurn === 'O') {
-                    playerO.win()
-                    playerO.scoreInfo()
-                }
-            }
+        if (turnCount < 5) return
+        switch (true) {
+            case checkWinner() && playerTurn === 'X':
+                pubSub.emit('playerWin', 'X')
+                pubSub.emit('info', 'X won this round !')
+                reset()
+                break
+            case checkWinner() && playerTurn === 'O':
+                pubSub.emit('playerWin', 'O')
+                pubSub.emit('info', 'O won this round !')
+                reset()
+                break
+            case checkWinner() === false && turnCount === 9:
+                pubSub.emit('info', 'Draw !')
+                reset()
+                break
         }
     }
 
-    function resetBoard() {
+    const reset =()=> {
         gameStats = [
             [null,null,null],
             [null,null,null],
             [null,null,null]
         ]
-        render()
+        turnCount = 0
+        setTimeout(()=>{
+            render()
+        },3000)
     }
 
-    function checkWinner() {
-        const lastMove = history[history.length-1]
+    const checkWinner =() =>{
+        const lastMove = players[turnCount-1]
         // check row and col
         for (let i = 0; i < gameStats.length; i++){
             checkCol:
@@ -73,118 +137,93 @@ const gameBoard = (()=> {
                 switch (true) {
                     case (gameStats[i][j] != lastMove):
                         break checkCol
-                    case j === gameStats[i].length-1:
-                        return true
-                }
-            }
-            checkRow:
-            for (let j = 0; j < gameStats[i].length; j++){
-                switch (true) {
-                    case (gameStats[j][i] != lastMove):
-                        break checkRow
-                    case j === gameStats[i].length-1:
-                        return true
-                }
+                        case j === gameStats[i].length-1:
+                            return true
+                        }
+                    }
+                    checkRow:
+                    for (let j = 0; j < gameStats[i].length; j++){
+                        switch (true) {
+                            case (gameStats[j][i] != lastMove):
+                                break checkRow
+                                case j === gameStats[i].length-1:
+                                    return true
+                                }
             }
             checkDia1:
             for (let j = 0; j < gameStats[i].length; j++){
                 switch (true) {
                     case (gameStats[j][j] != lastMove):
                         break checkDia1
-                    case j === gameStats[i].length-1:
-                        return true
-                }
-            }
-            checkDia2:
-            for (let j = 0; j < gameStats[i].length; j++){
-                switch (true) {
-                    case (gameStats[gameStats[i].length - 1 - j][j] != lastMove):
-                        break checkDia2
-                    case j === gameStats[i].length-1:
-                        return true
-                }
-            }
-        }
-        return false
+                        case j === gameStats[i].length-1:
+                            return true
+                        }
+                    }
+                    checkDia2:
+                    for (let j = 0; j < gameStats[i].length; j++){
+                        switch (true) {
+                            case (gameStats[gameStats[i].length - 1 - j][j] != lastMove):
+                                break checkDia2
+                                case j === gameStats[i].length-1:
+                                    return true
+                                }
+                            }
+                        }
+                        return false
     }
 
-    function announceWinner() {
-        const winner = history[history.length-1]
-        const newAnnouncement = document.createElement('div')
-        newAnnouncement.textContent = `${winner} WINS!`
-        announcementBoard.appendChild(newAnnouncement)
+    const blur = (arg) => {
+        board.classList.add('blur')
         setTimeout(()=>{
-            announcementBoard.removeChild(newAnnouncement)
-        },3000)
+            board.classList.remove('blur')
+        },arg)
     }
-
-    return {
+    
+    const preventClick = (arg) => {
+        board.removeEventListener('click', boardClickHandler)
+        setTimeout(()=>{
+            board.addEventListener('click', boardClickHandler)
+        },arg)
     }
-})()
-
-const infoBoard = (()=> {
-    // cache DOM
-    const announcementBoard = document.querySelector('.announcement')
-    const playerX = document.querySelector('.playerX')
-    const playerO = document.querySelector('.playerO')
 
     // bind events
-
-    render()
+    board.addEventListener('click', boardClickHandler)
+    pubSub.on('blur', blur)
+    pubSub.on('preventClick', preventClick)
     
-    // methods
-    function render() {
-        const 
-    }
+    // init
+    render()
     return {}
 })()
 
-
-const player = (arg) => {
-    const name = arg
-    let score = 0
-    const win = () => {
-        score++
+const infoBoard = (()=> {
+    let info = ''
+    
+    // cache DOM
+    const announcementBoard = document.querySelector('.announcement')
+    
+    // methods
+    const setInfo =(arg)=> {
+        info = arg
+        render()
     }
-    const scoreInfo = () => {
-        console.log(score)
+    
+    const render =()=> {
+        pubSub.emit('blur', 3000)
+        pubSub.emit('preventClick', 3000)
+        announcementBoard.textContent = info
+        setTimeout(()=>{
+            announcementBoard.textContent = ''
+        },3000)
     }
-    return {
-        scoreInfo,
-        win
-    }
-}
-
-const playerX = player('X')
-const playerO = player('O')
-
-console.log(playerX)
-const pubSub = (()=> {
-    let events = {}
-    const subscribe = (eventName, fn) => {
-      events[eventName] = events[eventName] || [];
-      events[eventName].push(fn);
-    }
-
-    const unsubscribe = (eventName, fn) => {
-      if (events[eventName]) {
-        for (let i = 0; i < events[eventName].length; i++) {
-          if (events[eventName][i] === fn) {
-            events[eventName].splice(i, 1);
-            break;
-          }
-        };
-      }
-    }
-
-    const publish = (eventName, data)=> {
-      if (events[eventName]) {
-        events[eventName].forEach((fn)=> {
-          fn(data);
-        });
-      }
-    }
-    return {
-        subscribe, unsubscribe, publish, events
-    }
+    
+    // bind events
+    pubSub.on('info', setInfo)
+    
+    // init
+    // render()
 })()
+
+const restartButton = () => {
+
+}
