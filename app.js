ea80a5b8601d4054df95fb23f9ca78e26443127f
vikyw89 <vikyw89@gmail.com>
2022-12-14 21:39:54 +0800

    all working, minimax and refactoring in progress

--- app.js
+++ app.js
@@ -1,354 +1,356 @@
-const pubSub = (() => {
-    let events = {}
+const player = (arg) => {
+    const _playerName = arg
+    let _brain = 'human'
+    let _score = 0
+
+    const _playerScore = document.querySelector(`.player-${_playerName.toLowerCase()}-score`)
+
+    const _render = () => {
+        _playerScore.textContent = _score
+    }
     
-    // methods
-    const on = (eventName, fn) => {
-      events[eventName] = events[eventName] || [];
-      events[eventName].push(fn);
+    const setScore = ()=> {
+        _score++
+        _render()
     }
-
-    const off = (eventName, fn) => {
-      if (events[eventName]) {
-        for (let i = 0; i < events[eventName].length; i++) {
-          if (events[eventName][i] === fn) {
-            events[eventName].splice(i, 1);
-            break;
-          }
-        }
-      }
+    
+    const setBrain = (arg) => {
+        return _brain = arg
     }
-
-    const emit = (eventName, data) => {
-      if (events[eventName]) {
-        events[eventName].forEach(function(fn) {
-          fn(data);
-        });
-      }
+    
+    const getBrain = () => {
+        return _brain
     }
 
     return {
-        on,
-        off,
-        emit
+        setBrain,
+        setScore,
+        getBrain
     }
-})()
-
-const player = (arg) => {
-    const name = arg
-    let brain = 'human'
-    let score = 0
-
-    // cache dom
-    const playerScore = document.querySelector(`.player-${name.toLowerCase()}-score`)
-
-    // methods
-    const render = () => {
-        playerScore.textContent = score
-    }
-
-    const playerMove = (arg) => {
-        if (arg !== name) return
-        switch (true) {
-            case brain === 'human':
-                console.log('human')
-                pubSub.emit('humanTurn')
-                break
-            case brain === 'easy-ai':
-                setTimeout(()=>{
-                    console.log('easy-ai')
-                    pubSub.emit('easyAiTurn')
-                },1000)
-                break
-            case brain === 'random-ai':
-                setTimeout(()=>{
-                    console.log('random-ai')
-                    pubSub.emit('randomAiTurn')
-                },1000)
-                break
-        }
-    }
-
-    const setScore = (winner)=> {
-        if (name === winner) {
-            score++
-            render()
-        }
-    }
-
-    const updateBrain = (arg) => {
-        brain = arg[name.toLowerCase()] ?? brain
-    }
-
-    // events handler
-
-    // bind events
-    pubSub.on('playerWin', setScore)
-    pubSub.on('playerTurn', playerMove)
-    pubSub.on('playerBrain', updateBrain)
-
-    // init
-    render()
 }
 
 const playerX = player('X')
 const playerO = player('O')
 
 const gameBoard = (()=> {
-    // variables
-    let gameStats = [
+    let _gameStats = [
         [null,null,null],
         [null,null,null],
         [null,null,null]
     ]
-    const players = ['X', 'O']
-    let turnCount = 0
+    const _players = ['X', 'O']
+    let _turnCount = 0
 
-    // cache dom
-    const board = document.querySelector('.tictactoe-container')
-    const cells = document.querySelectorAll('.cell')
+
+    const _board = document.querySelector('.tictactoe-container')
+    const _cells = document.querySelectorAll('.cell')
     
-    // methods
-    const render =()=> {
-        board.innerHTML = ''
-        for(let row = 0; row < gameStats.length; row++){
-            for (let col = 0; col < gameStats[row].length; col++){
+
+    const _render = () => {
+        _board.innerHTML = ''
+        for(let row = 0; row < _gameStats.length; row++){
+            for (let col = 0; col < _gameStats[row].length; col++){
                 const newCell = document.createElement('div')
                 newCell.setAttribute('data-col', col)
                 newCell.setAttribute('data-row', row)
                 newCell.classList.add('cell')
-                newCell.textContent = gameStats[row][col]
+                newCell.textContent = _gameStats[row][col]
                 if (newCell.textContent === 'X') {
                     newCell.setAttribute('class', 'font-effect-fire')
                 } else if (newCell.textContent === 'O') {
                     newCell.setAttribute('class', 'font-effect-neon')
                 }
-                board.appendChild(newCell)
+                _board.appendChild(newCell)
             }
         }
     }
-
-    const playerTurn = () => {
-        const turn = players[turnCount%2]
-        turnCount++
-        pubSub.emit('playerTurn', turn)
+    
+    const turnOf = (_turnCount) => {
+        return _players[_turnCount%2]
     }
 
-    const reset =()=> {
-        gameStats = [
+    const _playerTurn = () => {
+        const turn = turnOf(_turnCount)
+        _turnCount++
+        switch (true){
+            case turn === 'X' && playerX.getBrain() === 'human':
+                _enableClick()
+                break
+            case turn === 'O' && playerO.getBrain() === 'human':
+                _enableClick()
+                break
+            case turn === 'X' && playerX.getBrain() !== 'human':
+                ai.nextMove(playerX.getBrain())
+                break
+            case turn === 'O' && playerO.getBrain() !== 'human':
+                ai.nextMove(playerO.getBrain())
+                break
+        }
+    }
+    
+    const _enableClick = () => {
+        _board.addEventListener('click', _clickHandler)
+    }
+    
+    const _preventClick = () => {
+        _board.removeEventListener('click', _clickHandler)
+    }
+
+    const registeringPlayerMove = (arg) => {
+        const [row, col] = arg
+        if (_gameStats[row][col] !== null) return _enableClick()
+        const turn = _players[(_turnCount -1 )%2]
+        _gameStats[row][col] = turn
+        _render()
+        if (_turnCount < 5) return _playerTurn()
+        switch (true) {
+            case checkWinner(_gameStats, turn) && turn === 'X':
+                notification.setNotif('X WON THIS ROUND !')
+                playerX.setScore()
+                _reset()
+                break
+            case checkWinner(_gameStats, turn) && turn === 'O':
+                notification.setNotif('O WON THIS ROUND !')
+                playerO.setScore()
+                _reset()
+                break
+            case checkWinner(_gameStats, turn) === false && _turnCount === 9:
+                notification.setNotif('DRAW !')
+                _reset()
+                break
+            default:
+                _playerTurn()
+                break
+        }
+    }
+    
+    const checkWinner = (board, player) =>{
+        // check row and col
+        for (let i = 0; i < board.length; i++){
+            checkCol:
+            for (let j = 0; j < board[i].length; j++){
+                switch (true) {
+                    case (board[i][j] !== player):
+                        break checkCol
+                    case j === board[i].length-1:
+                        return true
+                }
+            }
+            checkRow:
+            for (let j = 0; j < board[i].length; j++){
+                switch (true) {
+                    case (board[j][i] !== player):
+                        break checkRow
+                    case j === board[i].length-1:
+                        return true
+                }
+            }
+            checkDia1:
+            for (let j = 0; j < board[i].length; j++){
+                switch (true) {
+                    case (board[j][j] !== player):
+                        break checkDia1
+                    case j === board[i].length-1:
+                        return true
+                }
+            }
+            checkDia2:
+            for (let j = 0; j < board[i].length; j++){
+                switch (true) {
+                    case (board[board[i].length - 1 - j][j] !== player):
+                        break checkDia2
+                    case j === board[i].length-1:
+                        return true
+                }
+            }
+        }
+        return false
+    }
+
+    const _reset =()=> {
+        _gameStats = [
             [null,null,null],
             [null,null,null],
             [null,null,null]
         ]
-        turnCount = 0
+        _turnCount = 0
+        _blurBoard()
         setTimeout(()=>{
-            render()
+            _render()
+            _playerTurn()
+            _unBlurBoard()
         },3000)
-        playerTurn()
     }
-
-    const checkWinner =() =>{
-        const lastMove = players[(turnCount-1)%2]
-        // check row and col
-        for (let i = 0; i < gameStats.length; i++){
-            checkCol:
-            for (let j = 0; j < gameStats[i].length; j++){
-                switch (true) {
-                    case (gameStats[i][j] != lastMove):
-                        break checkCol
-                        case j === gameStats[i].length-1:
-                            return true
-                        }
-                    }
-                    checkRow:
-                    for (let j = 0; j < gameStats[i].length; j++){
-                        switch (true) {
-                            case (gameStats[j][i] != lastMove):
-                                break checkRow
-                                case j === gameStats[i].length-1:
-                                    return true
-                                }
-            }
-            checkDia1:
-            for (let j = 0; j < gameStats[i].length; j++){
-                switch (true) {
-                    case (gameStats[j][j] != lastMove):
-                        break checkDia1
-                        case j === gameStats[i].length-1:
-                            return true
-                        }
-                    }
-                    checkDia2:
-                    for (let j = 0; j < gameStats[i].length; j++){
-                        switch (true) {
-                            case (gameStats[gameStats[i].length - 1 - j][j] != lastMove):
-                                break checkDia2
-                                case j === gameStats[i].length-1:
-                                    return true
-                                }
-                            }
-                        }
-                        return false
-    }
-
-    const blur = (arg) => {
-        board.classList.add('blur')
-        setTimeout(()=>{
-            board.classList.remove('blur')
-        },arg)
-    }
-
+    
     const getGameStats = () => {
-        return gameStats
+        return _gameStats
+    }
+    
+    const _blurBoard = () => {
+        _board.classList.add('blur')
     }
 
-    const registeringPlayerMove = (e) => {
-        board.removeEventListener('click', registeringPlayerMove)
-        if (gameStats[e.target.dataset.row][e.target.dataset.col] !== null) return waitingForMove()
-        const turn = players[(turnCount -1 )%2]
-        gameStats[e.target.dataset.row][e.target.dataset.col] = turn
-        render()
-        if (turnCount < 5) return playerTurn()
-        switch (true) {
-            case checkWinner() && turn === 'X':
-                pubSub.emit('playerWin', 'X')
-                pubSub.emit('info', 'X won this round !')
-                reset()
-                break
-            case checkWinner() && turn === 'O':
-                pubSub.emit('playerWin', 'O')
-                pubSub.emit('info', 'O won this round !')
-                reset()
-                break
-            case checkWinner() === false && turnCount === 9:
-                pubSub.emit('info', 'Draw !')
-                reset()
-                break
-            default:
-                playerTurn()
-                break
-        }
+    const _unBlurBoard = () => {
+        _board.classList.remove('blur')
+    }
+    
+    const resumeGame = () => {
+        _preventClick()
+        _turnCount--
+        _playerTurn()
     }
 
-    const waitingForMove = () => {
-        board.addEventListener('click', registeringPlayerMove)
+    const _clickHandler = (e) => {
+        _preventClick()
+        const [row, col] = [e.target.dataset.row, e.target.dataset.col]
+        registeringPlayerMove([row, col])
     }
 
-    // bind events
-    // board.addEventListener('click', boardClickHandler)
-    pubSub.on('blur', blur)
-    pubSub.on('playerTurn', waitingForMove)
-    pubSub.on('playerBrain', reset)
-
-    // init
-    render()
-    playerTurn()
+    _render()
+    _playerTurn()
 
     return {
-        getGameStats
+        getGameStats,
+        resumeGame,
+        registeringPlayerMove,
+        checkWinner,
+        turnOf
     }
 })()
 
-const easyAI = (() => {
-    // variables
+const ai = (() => {
+    let _availableMoves = []
+    let _selectedMoves = ''
 
-    // cacheDOM
-    // methods
-    const findNextMove = () => {
-        const gameStats = gameBoard.getGameStats()
+    const _updateAvailableMoves = (gameStats) => {
+        _availableMoves = []
         for (let i = 0; i < gameStats.length; i++){
             for (let j = 0; j < gameStats[i].length; j++){
                 if (gameStats[i][j] === null) {
-                    const aiNextMove = document.querySelector(`div[data-row='${i}'][data-col='${j}']`)
-                    pubSub.emit('playerTurn')
-                    aiNextMove.click()
-                    return
+                    const aiNextMove = [i,j]
+                    _availableMoves.push(aiNextMove)
                 }
             }
         }
-
+        return _availableMoves
     }
 
-    // bind events
-    pubSub.on('easyAiTurn', findNextMove)
-    // init
-})()
-
-const randomAI = (() => {
-    // variables
-    let gameStats = gameBoard.getGameStats()
-    let availableMoves = []
-    // cacheDOM
-    // methods
-    const findNextMove = () => {
-        console.log(availableMoves)
-
+    const nextMove = (arg) => {
+        _updateAvailableMoves(gameBoard.getGameStats())
+        switch (true) {
+            case arg === 'easy':
+                console.log('test')
+                _easy()
+                break
+            case arg === 'random':
+                _random()
+                break
+            case arg === 'impossible':
+                _impossible(gameBoard.getGameStats(), gameBoard.turnOf())
+                break
+        }
+        const [row, col] = [_selectedMoves[0], _selectedMoves[1]]
+        setTimeout(()=>{
+            gameBoard.registeringPlayerMove([row, col])
+        },1000)
     }
 
-    const updateAvailableMove = () => {
-        gameStats = gameBoard.getGameStats()
-        for (let i = 0; i < gameStats.length; i++){
-            for (let j = 0; j < gameStats[i].length; j++){
-                if (gameStats[i][j] === null) {
-                    availableMoves.push([i,j])
+    const _easy = () => {
+        console.log(_selectedMoves)
+        _selectedMoves = _availableMoves[0]
+    }
+
+    const _random = () => {
+        return _selectedMoves = _availableMoves[Math.floor(Math.random() * _availableMoves.length)]
+    }
+
+    const minMax = (board, isMaximizing, turn) => {
+        const value = {
+            true: 1,
+            false: -1,
+        }
+        let result = gameBoard.checkWinner(board, turn)
+        return -1
+    }
+
+    const _impossible = (board, turn) => {
+        console.log(board, turn)
+        let maxScore = -Infinity
+        let bestMove = []
+
+        for (let i = 0; i < board.length; i++) {
+            for (let j = 0; j < board[i].length; j++) {
+                if (board[i][j] === null) {
+                    board[i][j] = `${turn}`
+                    // check for score for particular move
+                    let score = minMax(board, true, turn)
+                    board[i][j] = null
+                    if (score > maxScore) {
+                        maxScore = score
+                        bestMove = [i,j]
+                    }
                 }
             }
         }
-        findNextMove()
+        return _selectedMoves = bestMove
     }
-
-    // bind events
-    pubSub.on('randomAiTurn', updateAvailableMove)
-
-    // init
+    return {
+        nextMove
+    }
 })()
 
-const infoBoard = (()=> {
+const notification = (()=> {
     // variables
-    let info = ''
+    let notif = ''
     
     // cache DOM
-    const announcementBoard = document.querySelector('.announcement')
+    const notifBoard = document.querySelector('.announcement')
     
     // methods
-    const setInfo =(arg)=> {
-        info = arg
-        render()
+    const setNotif =(arg)=> {
+        notif = arg
+        _render()
     }
     
-    const render =()=> {
-        pubSub.emit('blur', 3000)
-        pubSub.emit('preventClick', 3000)
-        announcementBoard.textContent = info
+    const _render =()=> {
+        notifBoard.textContent = notif
         setTimeout(()=>{
-            announcementBoard.textContent = ''
+            notifBoard.textContent = ''
         },3000)
     }
     
     // bind events
-    pubSub.on('info', setInfo)
-    
     // init
+    return {
+        setNotif
+    }
 })()
 
 const settings = (() => {
-    // variables
-    let playerXBrain = 'human'
-    let playerOBrain = 'human'
-
     // cache dom
-    const roleSelection = document.querySelectorAll('.settings > * > select')
+    const brainSelection = document.querySelectorAll('.settings > * > select')
 
-    // method
-    const updateBrain = (e) => {
+    // methods
+    const changeHandler = (e) => {
         const [player] = e.target.id.match(/(?<=[-])[ox](?=[-])/g)
         const brain = e.target.value
-        const updatedBrain = {[player]:brain}
-        pubSub.emit('playerBrain', updatedBrain)
+        switch (true) {
+            case player === 'x':
+                playerX.setBrain(brain)
+                gameBoard.resumeGame()
+                break
+            case player === 'o':
+                playerO.setBrain(brain)
+                gameBoard.resumeGame()
+                break
+        }
     }
 
     // bind events
-    roleSelection.forEach(element=> {
-        element.addEventListener('change', updateBrain)
+    brainSelection.forEach(element=> {
+        element.addEventListener('change', changeHandler)
     })
-    // init
+
+    return {
+    }
 })()
